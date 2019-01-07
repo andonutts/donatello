badc395b8e7e27108fb25ff7c57272899a6d2249
Andy He <smerchy@gmail.com>
2019-01-06 22:42:07 -0600

    Update application.js

--- js/application.js
+++ js/application.js
@@ -428,7 +428,7 @@ function adjustDeleteButtonVisibility() {
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
-    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.setSize( window.innerWidth, window.innerHeight, false );
 }
 
 function openControlPanel() {
