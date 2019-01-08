f884b7a974377fa57118cd340cb02537e3247ba3
Andy He <smerchy@gmail.com>
2019-01-07 20:56:43 -0600

    Testing changes

--- js/application.js
+++ js/application.js
@@ -445,10 +445,12 @@ function resizeRendererToDisplaySize(renderer) {
 
 function openControlPanel() {
     document.getElementById("menu").style.display = "block";
+    document.getElementById("canvas-container").style.display = "none";
 }
 
 function closeControlPanel() {
     document.getElementById("menu").style.display = "none";
+    document.getElementById("canvas-container").style.display = "block";
 }
 
 function generateHilbertCurve() {
